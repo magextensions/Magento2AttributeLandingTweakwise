c9de69ba7fcd5db2adb1622b5a17619931d22124
ah-net <103565001+ah-net@users.noreply.github.com>
2024-09-24 15:12:17 +0200

    Squashed commit of the following:
    commit 6c598078ec7c886c290434bc8cf7e9fb8fad23a9
    Merge: b087d6f 27b6967
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Tue Sep 24 14:39:53 2024 +0200
    
        Merge branch 'master' into beta
    
    commit b087d6f0b0242123e82f9ba54cd3c29a29c25ce2
    Merge: a63f1b7 cb61179
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Sep 19 11:36:00 2024 +0200
    
        Merge pull request #33 from EmicoEcommerce/fix-bump-php-version
    
        Fix: bump php version
    
    commit cb6117956af39f9eb5cc69204438728aa28d4007
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Sep 19 11:15:24 2024 +0200
    
        fix: bump php version
    
    commit a63f1b7a46361de41acc94ca80447acb83cbc448
    Merge: 7ce708c 15b7a61
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Sep 19 10:55:54 2024 +0200
    
        Merge pull request #31 from EmicoEcommerce/fix-return-landingpage-url
    
        Fix: return landingpage url
    
    commit 7ce708c5e21f2c9ef53344eb48a788f91cede8cc
    Merge: 69e0406 37a9fef
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Sep 19 10:46:29 2024 +0200
    
        Merge pull request #32 from EmicoEcommerce/fix-select-attribute
    
        fix: selecting attribute doesn't update
    
    commit 37a9fef8190d34da8603a6b9452b57f5b5eb07d2
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Tue Sep 17 10:46:38 2024 +0200
    
        fix: selecting attribute
    
    commit 15b7a612fbf0430c62ebb0bf704954bc9f39ebcd
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Fri Sep 13 13:18:12 2024 +0200
    
        Revert "Update RendererPlugin.php"
    
        This reverts commit b1d986bee9a803a782db2dde304dcdd3ac6d4482.
    
    commit b1d986bee9a803a782db2dde304dcdd3ac6d4482
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Fri Sep 13 13:02:54 2024 +0200
    
        Update RendererPlugin.php
    
    commit 9bd57b4b1f327beb80f2dc73c44da5914b7efeb6
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Jun 27 10:04:04 2024 +0200
    
        Remove space
    
    commit ce621b256b0b2ea0ca614690c0fafc1e55abf083
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Jun 27 10:01:38 2024 +0200
    
        Fix bug without ajax
    
    commit d50ae96ac6c9d39e68e0a413fccd1ad81ed3cd31
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Fri Jun 21 11:10:43 2024 +0200
    
        fix: landing page url
    
    commit 8bc79ac6aec7cd741346f36a9b81cac8805e025d
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Thu Jun 20 10:14:35 2024 +0200
    
        fix: bug with landingpage url
    
    commit 69e0406ca8037f982b9ce736fc670562047323dc
    Author: evs-xsarus <44158071+evs-xsarus@users.noreply.github.com>
    Date:   Tue Apr 30 15:58:23 2024 +0200
    
        Make installable on PHP 8.x (#24)
    
        * fix: make installable on php 8.3
    
    commit 9ac763e5cba93a567e1eb91c77903727aa6f056c
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Tue Apr 30 15:42:25 2024 +0200
    
        fix: save tweakwise filter template
    
    commit 103be57d9e245a7f9735a1ced4da43db14db809d
    Author: ah-net <103565001+ah-net@users.noreply.github.com>
    Date:   Tue Apr 30 15:41:50 2024 +0200
    
        chore: update version (#28)
    
        * chore: update version
    
        * chore: remove composer version
    
        * chore: support tweakwise 6.0
    
        * chore: support version 5.0 of attribute landing

--- src/Plugin/Block/LayeredNavigation/RenderLayered/RendererPlugin.php
+++ src/Plugin/Block/LayeredNavigation/RenderLayered/RendererPlugin.php
@@ -2,6 +2,7 @@
 
 namespace Tweakwise\AttributeLandingTweakwise\Plugin\Block\LayeredNavigation\RenderLayered;
 
+use Emico\AttributeLanding\Model\LandingPageContext;
 use Tweakwise\AttributeLandingTweakwise\Model\FilterManager;
 use Tweakwise\Magento2Tweakwise\Model\Catalog\Layer\Filter\Item;
 use Magento\Framework\View\Element\Template;
@@ -17,12 +18,18 @@ class RendererPlugin
     protected $filterManager;
 
     /**
+     * @var LandingPageContext
+     */
+    protected $landingPageContext;
+
+    /**
      * DefaultRendererPlugin constructor.
      * @param FilterManager $filterManager
      */
-    public function __construct(FilterManager $filterManager)
+    public function __construct(FilterManager $filterManager, LandingPageContext $landingPageContext)
     {
         $this->filterManager = $filterManager;
+        $this->landingPageContext = $landingPageContext;
     }
 
     /**
@@ -36,7 +43,18 @@ class RendererPlugin
         string $result,
         Item $filterItem
     ) {
-        if (!$this->filterManager->findLandingPageUrlForFilterItem($filterItem)) {
+
+        $returnToDefaultPage = false;
+
+        $landingPage = $this->landingPageContext->getLandingPage();
+        if ($landingPage) {
+            $landingPageUrl = $landingPage->getUrlPath();
+            if (stripos($result, $landingPageUrl) === false) {
+                $returnToDefaultPage = true;
+            }
+        }
+
+        if (!$this->filterManager->findLandingPageUrlForFilterItem($filterItem) && !$returnToDefaultPage) {
             return $result;
         }
 
